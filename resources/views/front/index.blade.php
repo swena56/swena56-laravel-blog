2d49e078e106a35a1443bf24617f3deb7b810781
= <swena56@gmail.com>
2016-06-11 23:21:47 -0500

    add background images

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,17 +1,22 @@
 @extends('front.template')
 
 @section('main')
-	<div class="row">
-		<div class="box">
-			<div class="col-lg-12">
-				<hr>
-				<h2 class="intro-text text-center"><strong>Andrew Swenson</strong></h2>
-				<hr>
-				<img class="img-responsive img-left" src="img/laravel-l-slant.png" alt="">
-			</div>
-		</div>
-	</div>
+        <div class="row">
+                <div class="box">
+                        <div class="col-lg-12">
+                                <hr>
+                                <h2 class="intro-text text-center"><strong>About Me</strong></h2>
+                                <hr>
+                                <img class="img-responsive img-left" src="img/laravel-l-slant.png" alt="">
+                                <p> Hello world, my name is Andrew Swenson. The Laravel framework has been a strong interest of mine for the past few weeks. There for my website will be contructed entirely with the Laravel Framework.  My first posts will primarly be about learning this incredible PHP framework, Laravel.</p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p>Ruby on Rails, you are dead to me!</p>
+                        </div>
+                </div>
+        </div>
 
 @stop
-
 
